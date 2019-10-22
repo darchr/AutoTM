7b944cc4ade7dfbf0845e83b3422b2a38eee5d6e
Mark Hildebrand <mhildebrand@ucdavis.edu>
2019-10-22 04:51:26 +0000

    Update 2LM runner

--- experiment-scripts/counters/run_2lm.sh
+++ experiment-scripts/counters/run_2lm.sh
@@ -1,40 +1,10 @@
-# Make sure `counters.jl` is running
+#!/bin/sh
+
+# Please make sure `counters.jl` is running
 
 ## Without scratchpad option
 # RW Counters
-julia --color=yes runner.jl --mode=2lm --workload=large_vgg --counter_type=rw
-julia --color=yes runner.jl --mode=2lm --workload=large_inception --counter_type=rw
-julia --color=yes runner.jl --mode=2lm --workload=large_resnet --counter_type=rw
-julia --color=yes runner.jl --mode=2lm --workload=large_densenet --counter_type=rw
-
-# Tag Counters
-julia --color=yes runner.jl --mode=2lm --workload=large_vgg --counter_type=tags
-julia --color=yes runner.jl --mode=2lm --workload=large_inception --counter_type=tags 
-julia --color=yes runner.jl --mode=2lm --workload=large_resnet --counter_type=tags 
-julia --color=yes runner.jl --mode=2lm --workload=large_densenet --counter_type=tags 
-
-# Queue counters
-julia --color=yes runner.jl --mode=2lm --workload=large_vgg --counter_type=queue
-julia --color=yes runner.jl --mode=2lm --workload=large_inception --counter_type=queue 
-julia --color=yes runner.jl --mode=2lm --workload=large_resnet --counter_type=queue 
-julia --color=yes runner.jl --mode=2lm --workload=large_densenet --counter_type=queue 
-
+julia --color=yes runner.jl --mode=2lm --workload large_vgg large_inception large resmet large_densenet --counter_type rw queues insert-check tags
 
 ## With scratchpad option
-# RW Counters
-julia --color=yes runner.jl --mode=2lm --workload=large_vgg --counter_type=rw --use_2lm_scratchpad
-julia --color=yes runner.jl --mode=2lm --workload=large_inception --counter_type=rw --use_2lm_scratchpad
-julia --color=yes runner.jl --mode=2lm --workload=large_resnet --counter_type=rw --use_2lm_scratchpad
-julia --color=yes runner.jl --mode=2lm --workload=large_densenet --counter_type=rw --use_2lm_scratchpad
-
-# Tag Counters
-julia --color=yes runner.jl --mode=2lm --workload=large_vgg --counter_type=tags --use_2lm_scratchpad
-julia --color=yes runner.jl --mode=2lm --workload=large_inception --counter_type=tags --use_2lm_scratchpad
-julia --color=yes runner.jl --mode=2lm --workload=large_resnet --counter_type=tags --use_2lm_scratchpad
-julia --color=yes runner.jl --mode=2lm --workload=large_densenet --counter_type=tags --use_2lm_scratchpad
-
-# Queue counters 
-julia --color=yes runner.jl --mode=2lm --workload=large_vgg --counter_type=queue --use_2lm_scratchpad
-julia --color=yes runner.jl --mode=2lm --workload=large_inception --counter_type=queue --use_2lm_scratchpad
-julia --color=yes runner.jl --mode=2lm --workload=large_resnet --counter_type=queue --use_2lm_scratchpad
-julia --color=yes runner.jl --mode=2lm --workload=large_densenet --counter_type=queue --use_2lm_scratchpad
+julia --color=yes runner.jl --mode=2lm --workload large_vgg large_inception large resmet large_densenet --counter_type rw queues insert-check tags --use_2lm_scratchpad
